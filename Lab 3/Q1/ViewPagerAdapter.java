71a693fc1d2e0ea0f2777c2277093c848ac53758
Richa Shanbhag <149705539+richashanbhag@users.noreply.github.com>
2026-02-02 19:15:51 +0530

    Create ViewPagerAdapter.java

---
+++ Lab 3/Q1/ViewPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+
+   public ViewPagerAdapter(@NonNull FragmentActivity fa) {
+       super(fa);
+   }
+
+
+   @NonNull
+   @Override
+   public Fragment createFragment(int position) {
+       if (position == 0) return new ArtistsFragment();
+       else if (position == 1) return new AlbumsFragment();
+       else return new SongsFragment();
+   }
+
+
+   @Override
+   public int getItemCount() {
+       return 3;
+   }
+}
