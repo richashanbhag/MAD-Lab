5cb699555666d03983ec42be60e372bb3810e800
Richa Shanbhag <149705539+richashanbhag@users.noreply.github.com>
2026-02-02 19:15:21 +0530

    Create SongsFragment.java

---
+++ Lab 3/Q1/SongsFragment.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+
+import android.os.Bundle;
+import android.view.*;
+import androidx.fragment.app.Fragment;
+
+
+public class SongsFragment extends Fragment {
+   @Override
+   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       return inflater.inflate(R.layout.fragment_songs, container, false);
+   }
+}
